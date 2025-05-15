4415315ffe3036583172e11478f429e1c4660412
Sam Tran <quocsam93@gmail.com>
2025-05-15 14:52:12 -0400

    inital commit

---
+++ init.py
@@ -0,0 +1,43 @@
+"""
+init.py
+
+Starting script to run NetPyNE-based M1 model.
+
+Usage:
+    python src/init.py # Run simulation, optionally plot a raster
+
+MPI usage:
+    mpiexec -n 4 nrniv -python -mpi src/init.py
+"""
+
+import matplotlib; matplotlib.use('Agg')  # to avoid graphics error in servers
+
+from netpyne import sim
+from neuron import h
+from cfg import cfg
+
+cfg, netParams = sim.loadFromIndexFile('index.npjson')
+
+# schedule application at runtime
+if not hasattr(cfg, 'afterSim'): cfg.afterSim = []
+def apply_tdcs_field():
+    amp = cfg.tDCS['amp']
+    ynormRange = cfg.tDCS.get('ynormRange', [0.0, 1.0])
+
+    for cell in sim.net.cells:
+        ynorm = cell.tags.get('ynorm', 0.5)
+        if not (ynormRange[0] <= ynorm <= ynormRange[1]):
+            continue
+        for secName, sec in cell.secs.items():
+            hSec = sec['hObj']
+            if 'extracellular' not in sec['mechs']:
+                sec['mechs']['extracellular'] = {}
+            for seg in hSec:
+                seg.e_extracellular = amp * ynorm
+
+# Attach to afterSim
+if not hasattr(cfg, 'afterSim'): cfg.afterSim = []
+cfg.afterSim.append(apply_tdcs_field)
+
+print("Starting sim ...")
+sim.createSimulateAnalyze(netParams, cfg)
